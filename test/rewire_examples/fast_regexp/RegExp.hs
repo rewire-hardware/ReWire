041501a76d7fcd6f04669b942f16c28ab99db7e9
Ian Graves <thegravian@gmail.com>
2014-02-20 15:00:16 -0600

    RegExp work

---
+++ test/rewire_examples/fast_regexp/RegExp.hs
@@ -0,0 +1,26 @@
+{-
+ -
+ -
+ - This implementation of a regular expression matching
+ - device is from Sidhu and Prasanna's "Fast Regular 
+ - Expression Matching using FPGAs".  
+ -
+ -}
+
+
+module RegExp where
+
+import Control.Monad.Resumption.Reactive
+import Control.Monad.Identity
+
+
+{- Here we define the abstract syntax for regular expressions -}
+
+data RegExp a =   Bar   (RegExp a) (RegExp a)
+                | Star  (RegExp a)
+                | Cons  (RegExp a) (RegExp a)
+                | Paren (RegExp a)
+                | Atom a
+
+
+type Machine input = ReacT input Bool Identity 
