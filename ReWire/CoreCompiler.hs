9262347887a5e9d3723b0b1223e3eb1261985fef
Adam Procter <amp269@gmail.com>
2014-02-24 16:43:18 -0600

    Added kind checker. Bundled everything up in a main function in CoreCompiler.hs (kind of a misnomer for the moment, because all it does is run things through parser, kind checker, type checker, and pretty-prints the result). Try loading CoreCompiler.hs into ghci and saying :main test/core_tests/tc.ncore

---
+++ ReWire/CoreCompiler.hs
@@ -0,0 +1,24 @@
+module ReWire.CoreCompiler where
+
+import System.IO
+import System.Environment
+import ReWire.Core
+import ReWire.CoreParser
+import ReWire.CoreKC
+import ReWire.CoreTC
+import ReWire.CorePP
+
+main :: IO ()
+main = do args <- getArgs
+          if length args /= 1
+             then do n <- getProgName
+                     hPutStrLn stderr $ "Syntax: " ++ n ++ " [filename.core]"
+             else do let filename = head args
+                     res_p <- parsefile filename
+                     case res_p of
+                       Left e  -> hPutStrLn stderr e
+                       Right p -> case kindcheck p of
+                                    Just e  -> hPutStrLn stderr e
+                                    Nothing -> case typecheck p of
+                                                 Left e   -> hPutStrLn stderr e
+                                                 Right p' -> print (pp p')
