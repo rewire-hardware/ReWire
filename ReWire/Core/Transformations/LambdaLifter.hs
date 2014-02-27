9e2826efcf2e4b4767dae8d84d3959ef390f346b
Ian Graves <thegravian@gmail.com>
2014-02-26 23:01:29 -0600

    More on the LL.

--- ReWire/Core/Transformations/LambdaLifter.hs
+++ ReWire/Core/Transformations/LambdaLifter.hs
@@ -3,10 +3,13 @@ module ReWire.Transformations.LambdaLifter where
 import ReWire.Core.Syntax
 import ReWire.Core.Transformations.Types
 import Unbound.LocallyNameless
+import Unbound.LocallyNameless.Types
 import Control.Monad
 import Control.Monad.Reader
 import Control.Monad.Writer
+import Control.Monad.State
 import Control.Monad.Identity
+import Data.List (nub)
 
 
 
@@ -15,7 +18,7 @@ import Control.Monad.Identity
 
 type ReaderEnv = ([(Name RWCExp,RWCTy)])
 
-type LLM = (WriterT [RWCDefn] (ReaderT ReaderEnv (FreshMT Identity)))
+type LLM = (WriterT [RWCDefn] (ReaderT ReaderEnv (StateT Int (LFreshMT Identity))))
 
 
 
@@ -27,7 +30,29 @@ ll_def defn = undefined
                                     return undefined
 
 ll_exp :: RWCExp -> LLM RWCExp
-ll_exp = undefined
+ll_exp l@(RWCLam _ _)  = do
+                          var <- build_defn_expr l
+                          var' <- wrap var
+                          error "Lambda Expressions need to added to environment to work" --return var'
+ll_exp (RWCApp ty e1 e2) = do
+                            e1' <- ll_exp e1
+                            e2' <- ll_exp e2
+                            return (RWCApp ty e1' e2')
+ll_exp v@(RWCVar _ _)    = return v
+ll_exp c@(RWCCon _ _)    = return c
+ll_exp l@(RWCLiteral _ _) = return l
+ll_exp (RWCCase ty e alts) = do
+                               e' <- ll_exp e
+                               alts' <- ll_alts alts
+                               return (RWCCase ty e' alts')
+
+ll_alts :: [RWCAlt] -> LLM [RWCAlt]
+ll_alts = mapM ll_alt
+
+ll_alt :: RWCAlt -> LLM RWCAlt
+ll_alt bnd = undefined -- do
+               --bnd' <- lunbind $ (\(pat,exp) -> do
+                                                    --exp' <- ll_exp
 
 
 {-| Peel off the first type (for a function)  and return the remaining type
@@ -47,8 +72,8 @@ peel_final_type ty = pft $ peel_first_type ty
 
 {-| Given some expression we are lifting out, this closes the current environment
  -  over it with applies -}
-close :: RWCExp -> LLM RWCExp
-close expr = do
+wrap :: RWCExp -> LLM RWCExp
+wrap expr = do
                env <- ask
                let closure = foldl (\acc (name,name_ty) -> 
                                         RWCApp (rest_ty acc) expr (RWCVar name_ty name)
@@ -58,11 +83,22 @@ close expr = do
      rest_ty expr = case peel_first_type $ rwc_expr_ty expr of --This will fail if we are ill-formed 
                             (_, Just rest) -> rest
 
---build_defn :: RWCExp -> LLM ()
---build_defn expr = do
---                    env <- ask
---                    let expr_ty = rwc_expr_ty expr
---                    let ek
+build_defn_expr :: RWCExp -> LLM (RWCExp)
+build_defn_expr expr = do
+                    env <- ask
+                    let expr_ty = rwc_expr_ty expr
+                        closure = foldl (\acc (name,name_ty) ->
+                                    (RWCLam 
+                                      (RWCTyApp (RWCTyApp (RWCTyCon "->") name_ty) expr_ty)
+                                        (bind name acc)
+                                       ) 
+                                        ) expr env
+                    lbl <- getLabel 
+                    let ctype = rwc_expr_ty closure
+                    let tys = nub $ rwc_ty_vars ctype --FIXME: Should we be nubbing this?
+                        defn = RWCDefn (s2n lbl) $ embed $ setbind tys (ctype,closure)
+                    tell [defn]
+                    return (RWCVar ctype $ s2n lbl)
 
 
 rwc_expr_ty (RWCApp ty _ _) = ty
@@ -72,3 +108,14 @@ rwc_expr_ty (RWCCon ty _)   = ty
 rwc_expr_ty (RWCLiteral ty _) = ty
 rwc_expr_ty (RWCCase ty _ _ ) = ty
 
+rwc_ty_vars :: RWCTy -> [Name RWCTy]
+rwc_ty_vars (RWCTyVar nme) = [nme]
+rwc_ty_vars (RWCTyCon _  ) = []
+rwc_ty_vars (RWCTyApp a1 a2) = (rwc_ty_vars a1) ++ (rwc_ty_vars a2)
+
+
+getLabel :: LLM String 
+getLabel = do
+              s <- get
+              put (s+1)
+              return ("#!ll" ++ (show s))
