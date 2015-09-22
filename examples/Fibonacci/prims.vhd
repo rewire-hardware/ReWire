3ea45fad9e8b01973402e32e8c0bd0e34b5aba54
Adam Procter <amp269@gmail.com>
2015-09-22 17:25:54 -0500

    Updating examples/restructuring the directory

---
+++ examples/Fibonacci/prims.vhd
@@ -0,0 +1,16 @@
+-- prims.vhd for Fibonacci.rw
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package prims is
+  pure function plusW8 (x : std_logic_vector; y : std_logic_vector) return std_logic_vector;
+end prims;
+
+package body prims is
+  pure function plusW8 (x : std_logic_vector; y : std_logic_vector) return std_logic_vector is
+  begin
+	return (std_logic_vector(unsigned(x)+unsigned(y)));
+  end plusW8;
+end prims;
