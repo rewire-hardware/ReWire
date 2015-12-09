b81a17321c7a856c8f0c15ee8d4da0449c210b0e
Adam Procter <amp269@gmail.com>
2015-12-08 21:35:01 -0600

    Basic parser tests

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -1,3 +1,22 @@
 import ReWire.Core.Parser
+import Test.Framework (defaultMain, testGroup, Test)
+import Test.Framework.Providers.HUnit
+import Test.HUnit hiding (Test)
+import Paths_ReWire
 
-main = putStrLn "welp!"
+-- TODO: should just snarf these from the dir listing
+filesToParse :: [FilePath]
+filesToParse = ["Fibonacci.rw","MiniISA.rw","Salsa20.rw","UpCounter.rw",
+                "dissex.rw","fibo.rw","funcase.rw","toag.rw",
+                "uniquification.rw"]
+
+testParse :: FilePath -> Test
+testParse f_ = testCase f_ (do f   <- getDataFileName ("test/parser_tests/" ++ f_)
+                               res <- parsefile f
+                               case res of
+                                 Left err -> assertFailure err
+                                 Right _  -> return ())
+
+tests = [testGroup "Files to Parse" (map testParse filesToParse)]
+
+main = defaultMain tests
