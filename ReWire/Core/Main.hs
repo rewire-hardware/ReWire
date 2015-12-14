53655c9d5b109b89573a25fbf41b0ec94e63039f
Adam Procter <amp269@gmail.com>
2015-12-14 17:27:30 -0600

    New shell interface
    You can now say rwc blah.rw -o blah.vhd to generate VHDL straight from the command line, or rwc -i blah.rw to get the old interactive loop. There are some other debug flags too---try typing just "rwc"

--- ReWire/Core/Main.hs
+++ ReWire/Core/Main.hs
@@ -11,6 +11,12 @@ import ReWire.Core.PrettyPrintHaskell
 import ReWire.Core.KindChecker
 import ReWire.Core.TypeChecker
 import ReWire.Core.Transformations.Interactive
+import ReWire.PreHDL.CFG (mkDot,gather,linearize,cfgToProg)
+import ReWire.PreHDL.GotoElim (gotoElim)
+import ReWire.PreHDL.ElimEmpty (elimEmpty)
+import ReWire.PreHDL.ToVHDL (toVHDL)
+import ReWire.Core.Transformations.ToPreHDL (cfgFromRW,eu)
+import Control.Monad (when)
 
 data Flag = FlagCFG String
           | FlagLCFG String
@@ -18,67 +24,98 @@ data Flag = FlagCFG String
           | FlagGPre String
           | FlagO String
           | FlagI
+          | FlagD
           deriving (Eq,Show)
 
 options :: [OptDescr Flag]
 options =
- [ Option ['i'] []       (NoArg FlagI)
-                         "run in interactive mode (overrides all other options)"
- , Option ['o'] []       (ReqArg FlagO "filename.vhd")
-                         "generate VHDL"
- , Option []    ["cfg"]  (ReqArg FlagCFG "filename.dot")
-                         "generate control flow graph before linearization"
- , Option []    ["lcfg"] (ReqArg FlagLCFG "filename.dot")
-                         "generate control flow graph after linearization"
- , Option []    ["pre"]  (ReqArg FlagPre "filename.phdl")
-                         "generate PreHDL before goto elimination"
- , Option []    ["gpre"] (ReqArg FlagGPre "filename.phdl")
-                         "generate PreHDL after goto elimination"
+ [ Option ['d'] ["debug"] (NoArg FlagD)
+                          "dump miscellaneous debugging information"
+ , Option ['i'] []        (NoArg FlagI)
+                          "run in interactive mode (overrides all other options)"
+ , Option ['o'] []        (ReqArg FlagO "filename.vhd")
+                          "generate VHDL"
+ , Option []    ["cfg"]   (ReqArg FlagCFG "filename.dot")
+                          "generate control flow graph before linearization"
+ , Option []    ["lcfg"]  (ReqArg FlagLCFG "filename.dot")
+                          "generate control flow graph after linearization"
+ , Option []    ["pre"]   (ReqArg FlagPre "filename.phdl")
+                          "generate PreHDL before goto elimination"
+ , Option []    ["gpre"]  (ReqArg FlagGPre "filename.phdl")
+                          "generate PreHDL after goto elimination"
  ]
 
 exitUsage :: IO ()
 exitUsage = hPutStr stderr (usageInfo "Usage: rwc [OPTION...] <filename.rw>" options) >> exitFailure
 
-main :: IO ()
-main = do args <- getArgs
-          let (flags,filenames,errs) = getOpt Permute options args
-          if not (null errs)
-             then mapM_ (hPutStrLn stderr) errs >> exitUsage
-             else if length filenames /= 1
-                     then hPutStrLn stderr "exactly one source file must be specified" >> exitUsage
-                     else do
-                       let filename = head filenames
-                       if FlagI `elem` flags
-                          then interactiveLoop filename
-                          else do
-                             res_p <- parseFile filename
-                             case res_p of
-                               ParseFailed loc m -> hPutStrLn stderr (prettyPrint loc ++ ":\n\t" ++ m) >> exitFailure
-                               ParseOk p ->
-                                 case kindcheck p of
-                                   Just e  -> hPutStrLn stderr e >> exitFailure
-                                   Nothing ->
-                                     case typecheck p of
-                                       Left e   -> hPutStrLn stderr e >> exitFailure
-                                       Right p' -> putStrLn "<<FIXME: bonk>>"
-
-interactiveLoop :: String -> IO ()
-interactiveLoop filename = do
+runFE :: Bool -> FilePath -> IO RWCProg
+runFE fDebug filename = do
   res_p <- parseFile filename
+
   case res_p of
-    ParseFailed loc m -> hPutStrLn stderr (prettyPrint loc ++ ":\n\t" ++ m) >> exitFailure
-    ParseOk p -> do
-      putStrLn "parse finished"
-      writeFile "show.out" (show p)
-      putStrLn "show out finished"
-      writeFile "Debug.hs" (show $ ppHaskellWithName p "Debug")
-      putStrLn "debug out finished"
+    ParseFailed loc m ->
+       hPutStrLn stderr (prettyPrint loc ++ ":\n\t" ++ m) >> exitFailure
+    ParseOk p         -> do
+
+      when fDebug $ do
+        putStrLn "parse finished"
+        writeFile "show.out" (show p)
+        putStrLn "show out finished"
+        writeFile "Debug.hs" (show $ ppHaskellWithName p "Debug")
+        putStrLn "debug out finished"        
+      
       case kindcheck p of
-        Just e  -> hPutStrLn stderr e
-        Nothing -> do putStrLn "kc finished"
-                      case typecheck p of
-                        Left e   -> hPutStrLn stderr e
-                        Right p' -> do putStrLn "tc finished"
-                                       writeFile "tc.out" (show p')
-                                       putStrLn "tc debug print finished"
-                                       trans p'
+        Just e  ->
+          hPutStrLn stderr e >> exitFailure
+        Nothing -> do
+          when fDebug (putStrLn "kc finished")
+          
+          case typecheck p of
+            Left e   -> hPutStrLn stderr e >> exitFailure
+            Right p' -> do
+
+              when fDebug $ do
+                putStrLn "tc finished"
+                writeFile "tc.out" (show p')
+                putStrLn "tc debug print finished"
+
+              return p'
+
+main :: IO ()
+main = do args                       <- getArgs
+
+          let (flags,filenames,errs) =  getOpt Permute options args
+          
+          when (not (null errs)) (mapM_ (hPutStrLn stderr) errs >> exitUsage)
+             
+          when (length filenames /= 1) (hPutStrLn stderr "exactly one source file must be specified" >> exitUsage)
+
+          let isActFlag (FlagCFG _)  = True
+              isActFlag (FlagLCFG _) = True
+              isActFlag (FlagPre _)  = True
+              isActFlag (FlagGPre _) = True
+              isActFlag (FlagO _)    = True
+              isActFlag FlagI        = True
+              isActFlag _            = False
+          
+          when (not (any isActFlag flags)) (hPutStrLn stderr "must specify at least one of -i, -o, --cfg, --lcfg, --pre, or --gpre" >> exitUsage)
+
+          let filename               =  head filenames
+
+          p <- runFE (FlagD `elem` flags) filename
+          
+          if FlagI `elem` flags then trans p
+          else do
+            let cfg     = cfgFromRW p
+                cfgDot  = mkDot (gather (eu cfg))
+                lcfgDot = mkDot (gather (linearize cfg))
+                pre     = cfgToProg cfg
+                gpre    = gotoElim pre
+                vhdl    = toVHDL (elimEmpty gpre)
+                doDump (FlagCFG f)  = writeFile f $ mkDot $ gather $ eu cfg
+                doDump (FlagLCFG f) = writeFile f $ mkDot $ gather $ linearize cfg
+                doDump (FlagPre f)  = writeFile f $ show pre
+                doDump (FlagGPre f) = writeFile f $ show gpre
+                doDump (FlagO f)    = writeFile f vhdl
+                doDump _            = return ()
+            mapM_ doDump flags
