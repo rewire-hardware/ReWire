0c881774b6813d1f3ac7a25d243e222b726bf1ec
William Harrison <bill@mwc-075121.dhcp.missouri.edu>
2016-07-21 15:15:06 -0500

    Just about done with the desugaring of record syntax.

--- ReWire/FrontEnd/Records.hs
+++ ReWire/FrontEnd/Records.hs
@@ -15,6 +15,154 @@ import ReWire.FrontEnd.Unbound
 import ReWire.Error
 import ReWire.FrontEnd.Syntax
 
+flatten :: [([a],b)] -> [(a,b)]
+flatten = foldr (\ (as,b) asbs -> cross b as ++ asbs) []
+      where cross :: b -> [a] -> [(a, b)]
+            cross b = foldr (\ a as -> (a,b) : as) [] 
+
+freshVar :: Fresh m => String -> m String
+freshVar n = fresh (string2Name $ "?X_" ++ n) >>= return . name2String
+
+freshVars :: (Enum a1, Num a1, Show a1, Fresh m) => a1 -> m [String]
+freshVars m = mapM (freshVar . show) [0..m-1]
+
+splitArrow :: (Fresh m, MonadError AstError m) => Poly -> m (Ty, Ty)
+splitArrow (Poly phi) = do
+  (_,ty) <- unbind phi
+  case ty of
+       (TyApp _ (TyApp _ (TyCon _ arr) t1) t2) -> return (t1,t2)
+       d                                       -> failAt (ann d) "record field non-arrow type"
+
+replaceAtIndex :: Int -> a -> [a] -> [a]
+replaceAtIndex n item ls = a ++ (item:b) where (a, (_:b)) = splitAt n ls
+
+mkApp :: Annote -> Exp -> [Exp] -> Exp
+mkApp an rator []         = rator
+mkApp an rator (arg:args) = mkApp an (App an rator arg) args
+
+--rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups ((x,e):xs) = case lookup x xs of
+                         Just _  -> rmdups xs
+                         Nothing -> (x,e) : rmdups xs
+
+----------------
+-- Desugaring variable and constructor record updates
+----------------
+
+findIndex :: Name FieldId -> Int -> [(Name FieldId, a)] -> Maybe Int
+findIndex f i []            = Nothing
+findIndex f i ((nf,_):rest) = if name2String f == name2String nf
+                               then Just i
+                               else findIndex f (i+1) rest
+
+-- is stuff like this kosher?
+poly2Ty :: Fresh m => Poly -> m Ty
+poly2Ty (Poly p) = do (_,ty) <- unbind p
+                      return ty
+
+lookUpF :: Eq a => a -> [(t, [(a, b)])] -> Maybe (t, [(a, b)])
+lookUpF f []           = Nothing
+lookUpF f ((d,fs):dfs) = case lookup f fs of
+                              Just _  -> Just (d,fs)
+                              Nothing -> lookUpF f dfs
+
+-- transRecUpdate is written under the assumption that the 'e' below is either a Var or a Con.
+-- I think that it's possible (have to check the parser) that it's a RecUp. I'm going to assume
+-- away the RecUp case for the time being.                      
+
+transRecUpdate :: (MonadError AstError m, Fresh m) =>
+                  Exp                                                      ->
+                  [(Embed (Name DataConId), [(Name FieldId, Embed Poly)])] ->
+                  m Exp
+transRecUpdate (RecUp an t e ups@((f,_):_)) rho = case e of
+  Var _ _ _  -> varrecupdate an (Embed t) c fs ups e
+     where Just (c,fs) = lookUpF f rho
+  Con _ t _  -> if length fs /= l_ups
+                   then 
+                     failAt an "Constructor record applications must be fully saturated in ReWire."
+                   else
+                     do exps <- arrange an fs ups'
+                        return $ mkApp an (Con an t c) exps
+     where Just (Embed c,fs) = lookUpF f rho
+           l_ups       = length ups'
+           ups'        = rmdups ups
+
+-- PU, this code stinks.
+
+arrange :: (MonadError AstError m, Eq a) => Annote -> [(a,b)] -> [(a,c)] -> m [c]
+arrange _ [] _            = return []
+arrange an ((f,_):fs) ups = case lookup f ups of
+                                 Just e -> do es <- arrange an fs ups'
+                                              return (e:es)
+                                     where ups' = filter (\ (f',e) -> f /= f') ups
+                                 Nothing -> failAt an "Failure processing constructor record application."
+
+varrecupdate :: Fresh m =>
+                Annote                       ->
+                Embed Ty                     ->
+                Embed (Name DataConId)       ->
+                [(Name FieldId, Embed Poly)] ->
+                [(Name FieldId, Exp)]        ->
+                Exp                          ->
+                m Exp
+varrecupdate an t d fs [] e           = return e
+varrecupdate an t c fs ((f,e'):ups) e = do e'' <- recupdate an t c (f,e') fs e 
+                                           varrecupdate an t c fs ups e''
+
+recupdate :: Fresh m =>
+             Annote                       ->
+             Embed Ty                     ->
+             Embed (Name DataConId)       ->
+             (Name FieldId,Exp)           ->
+             [(Name FieldId, Embed Poly)] ->
+             Exp                          ->
+             m Exp
+recupdate an ty@(Embed t) c (f,new) fds dscr = do
+  (p,e) <- mkRecPatExp an ty c f fds new
+  return (Case an t dscr (bind p e) Nothing)
+
+type DataEnv = [(Name DataConId, [(Name FieldId, Embed Poly)])]
+mkRecEnv :: [DataDefn] -> DataEnv
+mkRecEnv = foldr f []
+  where f d ds = case dataCons d of
+                      [RecCon an n t fds] -> (n,flatten fds) : ds
+                      _                   -> ds
+
+
+-- Given the declaration:  data T = C {f1,f2 :: Int}  
+-- Record Variable Update: x {f1 = 9}
+-- ...is desugared as:     case x of C d f2    -> C 9 f2
+mkRecPatExp
+  :: Fresh m =>
+     Annote                       ->
+     Embed Ty                     ->
+     Embed (Name DataConId)       ->
+     Name FieldId                 ->
+     [(Name FieldId, Embed Poly)] ->
+     Exp                          ->
+     m (Pat, Exp)
+mkRecPatExp an typ@(Embed ty) c@(Embed cstr) f fns e = do
+  fvs <- freshVars m
+  let fns'   = map ((\ (Embed x) -> x) . snd) fns
+  fns'' <- mapM poly2Ty fns'
+  let foobar = zip fvs fns''
+-- constructing the pattern (e.g., "C d f2")
+  let pvs    = map (\ (v,t) -> PatVar an (Embed t) (string2Name v)) foobar
+  let pat    = PatCon an typ c pvs
+-- constructing the expression (e.g., "C 9 f2")
+  let Just i = findIndex f 0 fns
+  let cvs    = map (\ (v,t) -> Var an t (string2Name v)) foobar
+  let t      = fns'' !! i
+  let cvs'   = replaceAtIndex i e cvs
+  let exp    = mkApp an (Con an ty cstr) cvs'
+  return (pat,exp)
+     where m = length fns
+
+----------------
+-- Desugaring data declarations with a single constructor that is also in record form                      
+----------------
+                         
 desugarRec :: (Fresh m, MonadError AstError m) => DataDefn -> m [Defn]
 desugarRec d = case dataCons d of
                     [RecCon an n t fds] -> recordDefs an n t fds
@@ -42,17 +190,6 @@ recordDefs an n (Embed (Poly phi)) flds = do
         fs    = map (string2Name . name2String . fst) fdecs
         tys   = map ((\ (Embed x) -> x) . snd) fdecs
         m     = length fdecs
-        flatten :: [([a],b)] -> [(a,b)]
-        flatten = foldr (\ (as,b) asbs -> cross b as ++ asbs) []
-            where cross :: b -> [a] -> [(a, b)]
-                  cross b = foldr (\ a as -> (a,b) : as) [] 
-
-splitArrow (Poly phi) = do
-  (_,ty) <- unbind phi
-  case ty of
-       (TyApp _ (TyApp _ (TyCon _ arr) t1) t2) -> return (t1,t2)
-       d                                       -> failAt (ann d) "record field non-arrow type"
-
 
 {-
 (mkdefn an f ty t p) creates abstract syntax for the following definition:
@@ -90,6 +227,8 @@ mkpats an t ts n m = map (mkpi an t ts n m) [0..m-1]
 data C = Car {company, boo :: String, model :: String, year :: Int} | Boat { etc :: Int } deriving (Show) 
 data S = S1 { x :: Int } | S2 { x :: Int }   -- OK  
 --data S' = S1' { x' :: Int } | S2' { x' :: Char }   -- Not OK since x' has different types.
+--data F = F { x :: Int }                            -- Not OK since x already field of S.
+
 
 --
 -- This is the definition arising from desugaring the year field of the Car constructor of the C type.
