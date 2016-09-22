0557f7a857f423d59ea381631181e23892692883
chathhorn <chathhorn@gmail.com>
2016-09-21 20:42:11 -0500

    Reorganizing tests.

--- test/TestMain.hs
+++ test/TestMain.hs
@@ -11,23 +11,13 @@ import Test.HUnit hiding (Test)
 
 import Paths_ReWire
 
-testFE :: FilePath -> FilePath -> Test
-testFE dn fn = testCase fn (do setCurrentDirectory dn
-                               lp  <- getSystemLoadPath
-                               res <- loadProgram lp fn
-                               case res of
-                                 Left e  -> assertFailure $ show e
-                                 Right m -> return ())
-
 testCompiler :: FilePath -> FilePath -> Test
-testCompiler dn fn = testCase fn (withArgs ["-o","/dev/null",(dn++"/"++fn)] M.main)
+testCompiler dn fn = testCase fn (do setCurrentDirectory dn
+                                     withArgs ["-o","/dev/null",(dn++"/"++fn)] M.main)
 
-tests = do dirname_fe <- getDataFileName ("test/frontend_tests")
-           files_fe   <- filter isHs <$> getDirectoryContents dirname_fe
-           dirname_c  <- getDataFileName ("test/compiler_tests")
+tests = do dirname_c  <- getDataFileName ("test/integration")
            files_c    <- filter isHs <$> getDirectoryContents dirname_c
-           return [testGroup "Frontend Tests"      (map (testFE dirname_fe) files_fe),
-                   testGroup "Full Compiler Tests" (map (testCompiler dirname_c) files_c)]
+           return [testGroup "\nFull Compiler Tests" (map (testCompiler dirname_c) files_c)]
    where isHs = (".hs" `isSuffixOf`)
 
 main = tests >>= defaultMain
