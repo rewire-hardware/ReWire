a150b37966453fc47ac80b0c85483a50bae8fc97
Chris Hathhorn <chris.hathhorn@twosixtech.com>
2023-02-06 21:04:03 -0500

    Add regression test for double signal bug.

---
+++ tests/regression/state0.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE DataKinds #-}
+import ReWire ( signal, Identity, ReacT )
+import ReWire.Bits ( W, lit )
+
+type Dev i o = ReacT i o Identity ()
+
+dev :: W 3 -> Dev (W 3) (Maybe (W 3))
+dev i = signal Nothing >> signal Nothing >> signal (Just i) >>= dev
+
+start :: Dev (W 3) (Maybe (W 3))
+start = dev (lit 0)
+
+main = undefined
