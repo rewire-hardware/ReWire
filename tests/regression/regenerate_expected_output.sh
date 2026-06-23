eefe973324fe287cfa432bf2b99fee7b53dd011d
Chris Hathhorn <hathhorn@gmail.com>
2026-06-23 13:15:15 -0500

    Update regenerate_expected_output.sh script.

--- tests/regression/regenerate_expected_output.sh
+++ tests/regression/regenerate_expected_output.sh
@@ -8,7 +8,11 @@ for file in ./*.hs; do
   ${RWC} --core ${file}
   echo $(basename ${file}) ": compile to SystemVerilog (${file/%.hs/.sv})"
   ${RWC} --from-core "${file/%.hs/.rwc}"
+  echo $(basename ${file}) ": compile to VHDL (${file/%.hs/.vhdl})"
+  ${RWC} --from-core --vhdl "${file/%.hs/.rwc}"
+  echo $(basename ${file}) ": compile to Cryptol (${file/%.hs/.cry})"
+  ${RWC} --from-core --cryptol "${file/%.hs/.rwc}"
   echo $(basename ${file}) ": interpret ReWire Core (${file/%.hs/.yaml})"
-  ${RWC} --from-core --interp "${file/%.hs/.rwc}"
+  ${RWC} --from-core --interp="${file/%.hs/.input.yaml}" "${file/%.hs/.rwc}"
 done
 echo "Done!"
